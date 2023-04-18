ec8ca2c15753f19434ed4003022bbcf3165f4a4c
Andres Mancilla Valdez <ga10026701@ga.com>
2023-04-18 15:49:22 -0700

    trying to figure out countdown with vue

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Oct 28, 2023 01:00:00").getTime();
+const app = Vue.createApp({
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+  data(){
+    return{
+      daysLeft: 0 // Initalize to 0
+    };
+  },
+  methods: {
+    calculateDaysLeft(){
+      // calculate how many days are left:
+      var countDownDate = new Date("Oct 28, 2023").getTime();
+      var now = new Date().getTime();
 
-  var now = new Date().getTime();
+      // milliseconds to days 
+      var days = (countDownDate - now) / 1000 / 60 / 60 / 24;
 
-  var distance = countDownDate - now;
-
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-
-  document.getElementById("demo").innerHTML =days + " DAYS TO GO!"
-
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "WE ARE NOW MARRIED";
+      // get whole days
+      this.daysLeft = Math.floor(days);
+    }
   }
-}, 1000);
+});
+
+app.mount('#weddingDate');
+
+
+// // Set the date we're counting down to
+// var countDownDate = new Date("Oct 28, 2023").getTime();
+
+// // Update the count down every 1 second
+// var x = setInterval(function() {
+
+//   var now = new Date().getTime();
+
+//   var distance = countDownDate - now;
+
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+//   document.getElementById("demo").innerHTML =days + " DAYS TO GO!"
+
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("demo").innerHTML = "WE ARE NOW MARRIED";
+//   }
+// }, 1000);
+
+
+
+
+
